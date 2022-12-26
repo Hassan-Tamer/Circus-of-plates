53ad6de63bb136b6e107c225f1c732a47a77721d
Hassan-Tamer <110876413+Hassan-Tamer@users.noreply.github.com>
2022-12-26 06:38:53 +0200

    Clown movement - Assets collected

--- Circus_Game/src/ShapeFactory.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-
-public class ShapeFactory {
-public static Shapes getShapes(String type, Color color){
-  if(type.equalsIgnoreCase("plate"))
-      return new Plate(color);
-  return null;
-}
-}
